d74c55de1c3083fc0b4ec31044a187118c92ee15
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-05-03 09:12:31 -0400

    initial checkin

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("CwsDemo")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CwsDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
