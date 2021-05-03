72ff6df1fcc2c8398f8b78b4d2613a8e08020535
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-05-03 13:39:22 -0400

    created HelloRequest

---
+++ HelloRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.WebScripting;
+
+namespace CwsDemo
+{
+    public class HelloRequest : IHttpCwsHandler
+    {
+        public void ProcessRequest(HttpCwsContext context)
+        {
+            var method = context.Request.HttpMethod;
+
+            switch (method)
+            {
+                case "GET":
+                    context.Response.StatusCode = 200;  // OK
+                    context.Response.Write("Hello World!", false);
+                    context.Response.End();
+                    break;
+                default:
+                    context.Response.StatusCode = 405;  // Method Not Allowed
+                    context.Response.End();
+                    break;
+            }
+        }
+    }
+}
