e2b0989f1a3c6cb4c64e7f41ce4472cf5512ffba
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-05-04 20:49:07 -0400

    switched to JSON and added a PUT handler

--- HelloRequest.cs
+++ HelloRequest.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
 using Crestron.SimplSharp.WebScripting;
+using Newtonsoft.Json;
 
 namespace CwsDemo
 {
     public class HelloRequest : IHttpCwsHandler
     {
+        private static string _name = "World";
+
         public void ProcessRequest(HttpCwsContext context)
         {
             var method = context.Request.HttpMethod;
@@ -14,19 +19,36 @@ namespace CwsDemo
             {
                 case "GET":
                     context.Response.StatusCode = 200;  // OK
+                    context.Response.ContentType = "application/json";
+
+                    var temp = new Dictionary<string, string>();
 
                     if (context.Request.RouteData.Values.ContainsKey("NAME"))
                     {
-                        var name = context.Request.RouteData.Values["NAME"];
-
-                        context.Response.Write(
-                            String.Format("Hello, {0}!", name),
-                            true);
+                        temp["name"] = context.Request.RouteData.Values["NAME"].ToString();
+                        context.Response.Write(JsonConvert.SerializeObject(temp), true);
                     }
                     else
                     {
-                        context.Response.Write("Hello World!", true);
+                        temp["name"] = _name;
+                        context.Response.Write(JsonConvert.SerializeObject(temp), true);
                     }
+                    break;
+                case "PUT":
+                    context.Response.StatusCode = 200;  // OK
+                    context.Response.ContentType = "application/json";
+
+                    using (var reader = new StreamReader(context.Request.InputStream))
+                    {
+                        var obj = JsonConvert.DeserializeObject<Dictionary<string, string>>(reader.ReadToEnd());
+                        
+                        if (obj.ContainsKey("name"))
+                        {
+                            _name = obj["name"];
+                            context.Response.Write("{ \"status\": \"OK\" }", true);
+                        }
+                    }
+
                     break;
                 default:
                     context.Response.StatusCode = 501;  // Not implemented
