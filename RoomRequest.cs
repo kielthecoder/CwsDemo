fd988b453160871ecfe6f3bfb49ec1d56a499f3f
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-05-10 17:32:37 -0400

    adding other RoomRequest routes

---
+++ RoomRequest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+using Crestron.SimplSharp.WebScripting;
+using Newtonsoft.Json;
+
+namespace CwsDemo
+{
+    public class RoomRequest : IHttpCwsHandler
+    {
+        private SystemSettings _settings;
+
+        public RoomRequest()
+        {
+            _settings = new SystemSettings();
+        }
+
+        public void ProcessRequest(HttpCwsContext context)
+        {
+            var method = context.Request.HttpMethod;
+
+            switch (method)
+            {
+                case "GET":
+                    context.Response.StatusCode = 200;  // OK
+                    context.Response.ContentType = "application/json";
+
+                    if (context.Request.RouteData.Values.ContainsKey("PROPERTY"))
+                    {
+                        var prop = context.Request.RouteData.Values["PROPERTY"];
+                        context.Response.Write(GetPropertyJSON(prop.ToString()), true);
+                    }
+                    else
+                    {
+                        context.Response.Write(JsonConvert.SerializeObject(_settings), true);
+                    }
+
+                    break;
+                case "PUT":
+                    break;
+                default:
+                    context.Response.StatusCode = 501;  // Not implemented
+                    context.Response.Write(
+                        String.Format("{0} method not implemented!", method),
+                        true);
+                    break;
+            }
+        }
+
+        public string GetPropertyJSON(string propName)
+        {
+            var obj = new Dictionary<string, string>();
+
+            switch (propName)
+            {
+                case "name":
+                    obj["value"] = _settings.name;
+                    break;
+                case "location":
+                    obj["value"] = _settings.location;
+                    break;
+                case "helpNumber":
+                    obj["value"] = _settings.helpNumber;
+                    break;
+            }
+
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
--- SystemSettings.cs
+++ SystemSettings.cs
@@ -7,30 +7,30 @@ namespace CwsDemo
         public const int MaxInputs = 8;
         public const int MaxOutputs = 8;
 
-        public string RoomName { get; set; }
-        public string Location { get; set; }
-        public string HelpNumber { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        public string helpNumber { get; set; }
 
-        public string[] InputName { get; set; }
-        public string[] OutputName { get; set; }
+        public string[] inputs { get; set; }
+        public string[] outputs { get; set; }
 
         public SystemSettings()
         {
-            InputName = new string[MaxInputs];
-            OutputName = new string[MaxOutputs];
+            inputs = new string[MaxInputs];
+            outputs = new string[MaxOutputs];
 
-            RoomName = "My Room";
-            Location = "My Office";
-            HelpNumber = "5555";
+            name = "My Room";
+            location = "My Office";
+            helpNumber = "5555";
 
             for (int i = 0; i < MaxInputs; i++)
             {
-                InputName[i] = String.Format("Input {0}", i + 1);
+                inputs[i] = String.Format("Input {0}", i + 1);
             }
 
             for (int i = 0; i < MaxOutputs; i++)
             {
-                OutputName[i] = String.Format("Output {0}", i + 1);
+                outputs[i] = String.Format("Output {0}", i + 1);
             }
         }
     }
