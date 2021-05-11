c10e2d85730b5d3bf6495974aaa074fd7b323dc9
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-05-11 15:19:41 -0400

    added stylesheet

--- html/app.js
+++ html/app.js
@@ -3,6 +3,7 @@ const locationInput = document.getElementById('location');
 const helpNumberInput = document.getElementById('helpNumber');
 
 const inputs = document.getElementById('inputs');
+const outputs = document.getElementById('outputs');
 
 async function getSettings () {
     let result = await fetch('/cws/api/room');
@@ -19,16 +20,40 @@ async function getSettings () {
         
         let label = document.createElement('label');
         label.setAttribute('for', `input-${i}`);
-        label.innerText = `Input ${i}`;
+        label.innerText = `Input ${i}: `;
         div.appendChild(label);
+        
+        div.appendChild(document.createTextNode(' '));
 
         let textField = document.createElement('input');
         textField.setAttribute('type', 'text');
-        textField.setAttribute('id', `input=${i}`);
+        textField.setAttribute('id', `input-${i}`);
         textField.setAttribute('value', item);
         div.appendChild(textField);
 
         inputs.appendChild(div);
+        i += 1;
+    });
+
+    i = 1;
+
+    data.outputs.forEach((item) => {
+        let div = document.createElement('div');
+        
+        let label = document.createElement('label');
+        label.setAttribute('for', `output-${i}`);
+        label.innerText = `Output ${i}: `;
+        div.appendChild(label);
+
+        div.appendChild(document.createTextNode(' '));
+
+        let textField = document.createElement('input');
+        textField.setAttribute('type', 'text');
+        textField.setAttribute('id', `output-${i}`);
+        textField.setAttribute('value', item);
+        div.appendChild(textField);
+
+        outputs.appendChild(div);
         i += 1;
     });
 }
